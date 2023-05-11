5a347a68b19c631ba757da4edae200d2fdddcdc3
Aluko Opeyemi Folajimi <62119915+Folexy13@users.noreply.github.com>
2023-05-11 12:30:14 +0100

    Create updateTransaction.js

---
+++ Controller/updateTransaction.js
@@ -0,0 +1,26 @@
+const stockModel = require("../Model/transaction.model");
+const updateStock = async function (req, res) {
+  try {
+    const { id, field,type } = req.body;
+    const updateStockLevel = await stockModel.findByIdAndUpdate(
+      { _id: id },
+     { { status: Number(field)  },
+      { new: true }
+    );
+    if (updateStockLevel) {
+      return res.status(200).send({
+        status: true,
+        message: "Update successfully",
+        data: updateStockLevel,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      status: false,
+      message: "An error occured",
+    });
+  }
+};
+
+module.exports = updateStock;
